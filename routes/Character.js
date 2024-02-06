f5fca3c51c8c90e48631e8c56838ddd5fa87ff0c
Manuel Frezzato <manuel.frezzato@gmail.com>
2024-02-06 12:44:21 +0100

    feat(Character.js): Impostate tutte le rotte ad esclusione del delete con gestione dell'errore

--- routes/Character.js
+++ routes/Character.js
@@ -1,38 +1,69 @@
 import express from "express";
 import Character from "../models/Character.js";
 
-const router = express.Router()
-//rotta Get Characters (lettura tutti personaggi)
+const router = express.Router();
+
+// Rotta Get Characters (lettura tutti i personaggi)
 router.get('/', async (req, res) => {
     try {
         const characters = await Character.find();
-        res.send(characters)
+        res.send(characters);
     } catch (err) {
-        res.status(500).send(err.message)
+        res.status(500).send(err.message);
     }
 });
 
-//rotta Get Character/:id (Personaggio specifico)
-router.get('/:id', (req, res) => {
-    const { id } = (req.params);
-    res.send(`Richiesta Personaggio ${id}`)
+// Rotta Get Character/:id (Personaggio specifico)
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const character = await Character.findById(id);
+        if (!character) {
+            res.status(404).send('Personaggio non trovato');
+        } else {
+            res.send(character);
+        }
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
 });
-//rotta Post Character (Creazione Personaggio)
-router.post('/', (req, res) => {
+
+// Rotta Post Character (Creazione Personaggio)
+router.post('/', async (req, res) => {
     const characterToCreate = req.body;
-    console.log('characterToCreate', characterToCreate);
-    res.status(201).send(characterToCreate)
-})
-//rotta Put Character (Modifica Personaggio)
-router.put('/:id', (req, res) => {
+    try {
+        const character = await Character.create(characterToCreate);
+        res.status(201).send(character);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
+
+// Rotta Put Character (Modifica Personaggio)
+router.put('/:id', async (req, res) => {
     const { id } = req.params;
     const characterToUpdate = req.body;
-    res.status(200).send(characterToUpdate)
+    try {
+        const updatedCharacter = await Character.findByIdAndUpdate(id, characterToUpdate, { new: true });
+        if (!updatedCharacter) {
+            res.status(404).send('Personaggio non trovato');
+        } else {
+            res.send(updatedCharacter);
+        }
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
 
-})
-//rotta Delete Character/:id ( Eliminazione Personaggio specifico)
-router.delete('/:id', (req, res) => {
+// Rotta Delete Character/:id (Eliminazione Personaggio specifico)
+router.delete('/:id', async (req, res) => {
     const { id } = req.params;
-    res.status(204).send()
-})
+    try {
+        await Character.findByIdAndDelete(id);
+        res.status(204).send();
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
+
 export default router;
